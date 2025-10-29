8ce36b147a58e5599257f03f416580d9885dae71
Irham Maulana <irhammaulana011@gmail.com>
2025-10-29 09:56:43 +0700

    api-nav\n- header\n-footer

--- config/database.ts
+++ config/database.ts
@@ -27,9 +27,9 @@ export default ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'my_strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'jason404'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
---
+++ types/generated/components.d.ts
@@ -0,0 +1,184 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ElementCard extends Struct.ComponentSchema {
+  collectionName: 'components_element_cards';
+  info: {
+    displayName: 'Card';
+  };
+  attributes: {};
+}
+
+export interface LayoutBanner extends Struct.ComponentSchema {
+  collectionName: 'components_layout_banners';
+  info: {
+    displayName: 'Banner';
+    icon: 'apps';
+  };
+  attributes: {
+    isVisible: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.Component<'shared.link', false>;
+  };
+}
+
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+    icon: 'crown';
+  };
+  attributes: {
+    Logo: Schema.Attribute.Component<'shared.logo-link', false>;
+    navItems: Schema.Attribute.Component<'shared.link', true>;
+    socialLinks: Schema.Attribute.Component<'shared.logo-link', true>;
+    text: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'shared.link', false>;
+    logo: Schema.Attribute.Component<'shared.logo-link', false>;
+    navItems: Schema.Attribute.Component<'shared.link', true>;
+  };
+}
+
+export interface MenuDropdownn extends Struct.ComponentSchema {
+  collectionName: 'components_menu_dropdownns';
+  info: {
+    displayName: 'Dropdownn';
+    icon: 'command';
+  };
+  attributes: {
+    description: Schema.Attribute.String;
+    href: Schema.Attribute.String;
+    label: Schema.Attribute.String;
+  };
+}
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    description: Schema.Attribute.String;
+    href: Schema.Attribute.String;
+    isButtonLink: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+  };
+}
+
+export interface SharedLogoLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_logo_links';
+  info: {
+    displayName: 'Logo ';
+    icon: 'crown';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+  };
+}
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedNav extends Struct.ComponentSchema {
+  collectionName: 'components_shared_navs';
+  info: {
+    displayName: 'Navigation';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean;
+    label: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<['PRIMARY']>;
+  };
+}
+
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'element.card': ElementCard;
+      'layout.banner': LayoutBanner;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
+      'menu.dropdownn': MenuDropdownn;
+      'shared.link': SharedLink;
+      'shared.logo-link': SharedLogoLink;
+      'shared.media': SharedMedia;
+      'shared.nav': SharedNav;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
+    }
+  }
+}
